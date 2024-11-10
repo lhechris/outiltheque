1ffe36b7dd10834c8efee6adee1989b69178bcd5
LHERMITTE Christophe <christophe.lhermitte@viveris.fr>
2024-11-10 16:56:44 +0100

    correction bug dans helper.js

--- resources/js/helper.js
+++ resources/js/helper.js
@@ -2,32 +2,32 @@ import axios from "axios";
 
 export const request = async (method, url, data) => {
     const token = localStorage.getItem('APP_DEMO_USER_TOKEN')
+    let headers = null;
     if (token !== undefined && token !== "" && token !== null) {
-        const headers = {
+        headers = {
             headers: {
                 Authorization: 'Bearer ' + token
             }
         }
-        let response = null;
-        switch (method) {
-            case 'get':
-                response = await axios.get(url, headers)
-                break;
-            case 'post':
-                response = await axios.post(url, data, headers)
-                break;
-            case 'put':
-                response = await axios.put(url, data, headers)
-                break;
-            case 'delete':
-                response = await axios.delete(url, headers)
-                break;
-            default:
-                break;
-        }
-        return response
     }
-    return false
+    let response = null;
+    switch (method) {
+        case 'get':
+            response = await axios.get(url, headers)
+            break;
+        case 'post':
+            response = await axios.post(url, data, headers)
+            break;
+        case 'put':
+            response = await axios.put(url, data, headers)
+            break;
+        case 'delete':
+            response = await axios.delete(url, headers)
+            break;
+        default:
+            break;
+    }
+    return response
 }
 
 export const upload_file = async (url, file) => {
